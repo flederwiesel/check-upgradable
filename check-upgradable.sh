dad5cc6efe6b5cfb72cd6da1fc009f36d283355c
flederwiesel <flederwiesel@fra-flugplan.de>
2021-10-25 14:34:55 +0200

    Update 7+ Taskbar Tweaker, Google Chrome, Notepad++, VirtualBox and XAMPP.

--- check-upgradable.sh
+++ check-upgradable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apps=(
-	# 7+ Taskbar Tweaker v5.11.3
-	"7+ Taskbar Tweaker,09d31d22178238a329aa6832d492fd6e,https://rammichael.com/downloads/7tt_setup.exe?changelog"
+	# 7+ Taskbar Tweaker v5.12.1
+	"7+ Taskbar Tweaker,c83edb24e5c6c4f6a4793f198bf1d7a5,https://rammichael.com/downloads/7tt_setup.exe?changelog"
 	# Adobe Reader 2021.007.20099
 	"Adobe Reader,7fab855641eee46ef4d03b0d386dd741,https://admdownload.adobe.com/bin/live/readerdc64_en_xa_install.exe"
 	# Apple Mobile Device Support 15.0.0.16
@@ -18,8 +18,8 @@ apps=(
 	"GIMP,4b0b919cd49fe2aac5469ec14d8fd9eb,https://www.gimp.org/downloads/"
 	# Go 1.17.2
 	"Go Compiler,3f79343284324e8be42f22a63d1aba07,https://golang.org/dl/"
-	# 95.0.4638.49 (Oct. 13, 2021)
-	"Google Chrome,dfbb1c61381a1bdd6575b3948309abb7,https://latestversionof.com/chrome/"
+	# 96.0.4664.18 (Oct. 21, 2021)
+	"Google Chrome,36498a6337566c0d82b8125be77d16c8,https://latestversionof.com/chrome/"
 	# 2.24.33-2021-01-30
 	# 3.24.24-2021-01-30
 	"gtk2-runtime,bf989de4faf29a08b39ae207dd6c9c49,https://api.github.com/repos/tschoonj/GTK-for-Windows-Runtime-Environment-Installer/releases/latest| jq -r .published_at"
@@ -29,8 +29,8 @@ apps=(
 	"KeePass,b013b88d2d5b9a15b9fc9d4462260b49,https://keepass.info/download.html"
 	# Firefox 3.0
 	"Mozilla Firefox,eb93daf279690f11923017890071a3de,https://download.mozilla.org/?product=firefox-latest&os=win&lang=en-US,Firefox-Setup.exe"
-	# Notepad++ 8.1.6
-	"Notepad++,a7e341e898ab2b9692c9183892c7185c,https://notepad-plus-plus.org/downloads/"
+	# Notepad++ 8.1.9
+	"Notepad++,1c364b3b3ca764d748d1b88799bb965b,https://notepad-plus-plus.org/downloads/"
 	# Paint.NET 4.3.2
 	"Paint.NET,01708a41ab0942be865b941603e15e08,https://www.getpaint.net/download.html"
 	# PCANView 4.3.4.615
@@ -54,16 +54,16 @@ apps=(
 	"Unison,4832ba2e4c1ae1f093c0d65c450b4f66,https://api.github.com/repos/bcpierce00/unison/releases/latest| jq -r .published_at"
 	# VeraCrypt 1.24-Update8 (Saturday November 28, 2020)
 	"VeraCrypt,fcb084a4a8940fceb45a13a9947bee99,https://www.veracrypt.fr/en/Downloads.html"
-	# VirtualBox 6.1.26
-	"VirtualBox,e6c06433dcb7bcf561969566b4e85b98,https://www.virtualbox.org/wiki/Downloads"
+	# VirtualBox 6.1.28
+	"VirtualBox,0adaa84fa5ee6f9c0b38631ec5b62fe7,https://www.virtualbox.org/wiki/Downloads"
 	# VirtuaWin 4.5
 	"VirtuaWin,7b8395abcd47ee2bc71ab08ccad02308,https://virtuawin.sourceforge.io/?page_id=2"
 	# VLC Media Player 3.0.16
 	"VLC Media Player,770ab3b4a6dd0ba206ebf5b884009578,https://api.github.com/repos/videolan/vlc/tags"
 	# Wireshark 3.4.9
 	"Wireshark,d45b60f31e6163246e143df9b00bfba8,https://1.eu.dl.wireshark.org/win64/"
-	# XAMPP 8.0.11
-	"XAMPP,095b45ecdb0e46971ab716348082bc73,https://www.apachefriends.org/download.html"
+	# XAMPP 8.0.12
+	"XAMPP,7dd42f4ef0c65fc7f81b57cd05600eab,https://www.apachefriends.org/download.html"
 	# yEd 3.21.1
 	"yEd,9688eb8ba62cecb318a1059b0b87ad37,https://www.yworks.com/products/yed/download%23download"
 )
